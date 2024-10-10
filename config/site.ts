26492789f5a36842810ec41151b927f9065858f1
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-10-10 12:26:04 +0700

    feat(config): add website configuration

---
+++ config/site.ts
@@ -0,0 +1,5 @@
+export const siteConfig = {
+  name: "Evolve",
+  description:
+    "Evolve is a platform that explores the theory of evolution, diving deep into the evolution of humans, animals, and the fundamental concepts of evolution itself.",
+};
