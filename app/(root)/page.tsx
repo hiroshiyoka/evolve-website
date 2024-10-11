f472a6f9d93d6f192728350d956cd106b880fcb5
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-10-12 04:06:55 +0700

    feat(app): add update to the home page style

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,9 +2,11 @@ import Hero from "@/components/hero";
 
 const Home = () => {
   return (
-    <main className="relative bg-neutral-900 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-3xl w-full">
-        <Hero />
+    <main className="relative bg-neutral-900 min-h-screen overflow-hidden">
+      <div className="h-screen flex items-center justify-center sm:px-10 px-5">
+        <div className="max-w-3xl w-full">
+          <Hero />
+        </div>
       </div>
     </main>
   );
