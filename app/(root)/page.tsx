0d12163a3d78dd509b0ff3d5fe901cf28aa458e6
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-10-13 02:25:55 +0700

    feat(app): add update to the home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,17 @@
-import Hero from "@/components/hero";
+import Hero from "./_components/hero";
+import Resume from "./_components/resume";
 
 const Home = () => {
   return (
-    <main className="relative bg-neutral-900 min-h-screen overflow-hidden">
-      <div className="h-screen flex items-center justify-center sm:px-10 px-5">
+    <main className="relative bg-neutral-900 overflow-y-auto overflow-x-hidden">
+      <section className="h-screen flex items-center justify-center sm:px-10 px-5">
         <div className="max-w-3xl w-full">
           <Hero />
         </div>
-      </div>
+      </section>
+      <section className="h-screen flex items-center justify-center sm:px-10 px-5">
+        <Resume />
+      </section>
     </main>
   );
 };
