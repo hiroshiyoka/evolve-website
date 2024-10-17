0ed468b133947cce46d1de6b2f4c5a725f5028c0
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-10-18 02:37:47 +0700

    feat(app): add timeline section component

---
+++ app/(root)/_components/timeline-section.tsx
@@ -0,0 +1,11 @@
+import { Timeline } from "@/components/ui/timeline";
+
+const TimelineSection = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <Timeline />
+    </div>
+  );
+};
+
+export default TimelineSection;
